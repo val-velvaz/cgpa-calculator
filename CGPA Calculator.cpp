1ff8cfb09e4ab96548bd396d80b7ab65fb429765
val-velvaz <valevelazquezvazquez@gmail.com>
2026-05-10 15:45:51 -0600

    Add project files.

---
+++ CGPA Calculator.cpp
@@ -0,0 +1,12 @@
+﻿// CGPA Calculator.cpp : Defines the entry point for the application.
+//
+
+#include "CGPA Calculator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
